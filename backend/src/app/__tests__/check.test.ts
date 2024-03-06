4a7a667a610c2ad7fdde707dd2f25c442619213a
Alexandre Bedel <alexandre.bedel@epitech.eu>
2024-03-06 22:29:04 +0100

    Fixed issues on the test suite & added default tests on the check route

--- backend/src/app/__tests__/check.test.ts
+++ backend/src/app/__tests__/check.test.ts
@@ -1,18 +1,55 @@
-import { createMocks } from "node-mocks-http";
-import { POST as POSTHandler } from "../api/check/route";
+/**
+ * @jest-environment node
+ */
+import { testApiHandler } from "next-test-api-route-handler";
+import * as appHandler from "../api/check/route";
+
+const errorKeys = ["message", "error", "instance"] as const;
 
 describe("/api/check", () => {
   test("Make sure the route is only a POST", async () => {
-    const { req, res } = createMocks({ method: "GET" });
-    // @ts-ignore
-    const response = await POSTHandler(req);
+    await testApiHandler({
+      appHandler,
+      async test({ fetch }) {
+        const res = await fetch({ method: "GET" });
 
-    console.log(response);
-    expect(1).toBe(0);
-    // expect({}).toBe(200)
-    // expect(await response.json()).toEqual([
-    //   { id: 1, name: 'Alice' },
-    //   { id: 2, name: 'Bob' },
-    // ])
+        expect(res.status).toBe(405);
+        expect(res.statusText).toBe("Method Not Allowed");
+      },
+    });
+  });
+
+  // const form = new FormData();
+
+  // form.append('image', '');
+  // const res = await fetch({ method: "POST", body: for });
+
+  test("Should return an error if there's no POST body", async () => {
+    await testApiHandler({
+      appHandler,
+      async test({ fetch }) {
+        const res = await fetch({ method: "POST" });
+        const body = await res.json();
+
+        expect(res.status).toBe(400);
+        expect(Object.keys(body)).toEqual(expect.arrayContaining(errorKeys));
+      },
+    });
+  });
+
+  test("Should return an error if this is not an image on the form data", async () => {
+    await testApiHandler({
+      appHandler,
+      async test({ fetch }) {
+        const form = new FormData();
+
+        form.append("image", "");
+        const res = await fetch({ method: "POST", body: form });
+        const body = await res.json();
+
+        expect(res.status).toBe(400);
+        expect(Object.keys(body)).toEqual(expect.arrayContaining(errorKeys));
+      },
+    });
   });
 });
