34da414b94e99e1f0778cc633fb3c08fc30d63a4
Alexandre Bedel <alexandre.bedel@epitech.eu>
2024-03-07 19:12:26 +0100

    #14 Improved hardware code & almost sending the image to the backend

--- iot/src/main.cpp
+++ iot/src/main.cpp
@@ -1,67 +1,50 @@
 #include <M5Stack.h>
-#include "FS.h"
-#include "SPIFFS.h"
-#include "uart_frame.h"
 #include "core.h"
+#include "camera.h"
+#include "filesystem.h"
+#include "network.h"
+#include "UDHttp.h"
 
-static QueueHandle_t jpeg_frame_queue = NULL;
-
-void frame_recv_callback(int cmd, const uint8_t *buf, int len)
-{
-  JpegFrame_t frame;
-
-  frame.buf = (uint8_t *)malloc(sizeof(uint8_t) * len);
-  memcpy(frame.buf, buf, len);
-  frame.size = len;
-  if (xQueueSend(jpeg_frame_queue, &frame, 0) != pdTRUE)
-  {
-    free(frame.buf);
-  }
-}
+unsigned long lastPictureTime = 0;
+const unsigned long pictureInterval = 6000;
+bool runOnce = false;
 
 void setup()
 {
-  M5.begin(true, false, false, false);
-  Serial.begin(9200);
-  if (!SPIFFS.begin(true))
-  {
-    Serial.println("Failed to begin splifff my man");
-    return;
-  }
-  uart_frame_init(36, 26, 1500000);
-  jpeg_frame_queue = xQueueCreate(2, sizeof(JpegFrame_t));
-  pinMode(16, INPUT);
+  M5.begin();
+  Serial.begin(9600);
+  init_network();
+  Filesystem::init();
+  Camera::init();
+  lastPictureTime = millis();
 }
-
-bool disablePhoto = false;
 
 void loop()
 {
-  static bool photo_taken = false;
-  static uint32_t photo_timestamp = 0;
-  if (!photo_taken)
-  {
-    if (millis() - photo_timestamp >= 1)
-    {
-      photo_taken = true;
-      Serial.println("Taking photo...");
-    }
-  }
+  unsigned long currentTime = millis();
 
-  if (digitalRead(16) == 1 && photo_taken && !disablePhoto)
+  if (currentTime - lastPictureTime >= pictureInterval && !runOnce)
   {
-    JpegFrame_t frame;
-
-    disablePhoto = true;
-    if (xQueueReceive(jpeg_frame_queue, &frame, portMAX_DELAY) == pdTRUE)
-    {
-      M5.Lcd.drawJpg(frame.buf, frame.size, 0, 0);
-      Serial.printf("Received frame - size: %d\n", frame.size);
-      // saveJpegToSPIFFS(frame.buf, frame.size, "/image.jpg");
-      // listFiles("/");
-      free(frame.buf);
-      photo_taken = false;
-      photo_timestamp = millis();
-    }
+    Serial.println("Taking a picture");
+    Camera::savePicture();
+    lastPictureTime = currentTime;
+    runOnce = true;
   }
 }
+
+// void loop()
+// {
+//   // put your main code here, to run repeatedly:
+//   UDHttp udh;
+//   // open file on sdcard to read
+//   root = SPIFFS.open("test.pdf");
+//   if (!root)
+//   {
+//     Serial.println("can not open file!");
+//     return;
+//   }
+//   // upload downloaded file to local server
+//   udh.upload("http://192.168.1.107:80/upload/upload.php", "test.pdf", root.size(), rdataf, progressf, responsef);
+//   root.close();
+//   Serial.printf("done uploading\n");
+// }
