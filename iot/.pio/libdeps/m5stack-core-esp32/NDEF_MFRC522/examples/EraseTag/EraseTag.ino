b01050cc85177bd938582c5f209cc8aaf2772353
Alexandre Bedel <alexandre.bedel@epitech.eu>
2024-03-08 15:34:53 +0100

    Trying to fix folder casing

---
+++ iot/.pio/libdeps/m5stack-core-esp32/NDEF_MFRC522/examples/EraseTag/EraseTag.ino
@@ -0,0 +1,33 @@
+// Erases a NFC tag by writing an empty NDEF message 
+
+#include <SPI.h>
+#include <MFRC522.h>
+#include "NfcAdapter.h"
+
+#define SS_PIN D8
+
+MFRC522 mfrc522(SS_PIN, UINT8_MAX); // Create MFRC522 instance
+
+NfcAdapter nfc = NfcAdapter(&mfrc522);
+
+void setup(void) {
+    Serial.begin(9600);
+    Serial.println("NFC Tag Eraser");
+    nfc.begin();
+}
+
+void loop(void) {
+    Serial.println("\nPlace a tag on the NFC reader to erase.");
+
+    if (nfc.tagPresent()) {
+
+        bool success = nfc.erase();
+        if (success) {
+            Serial.println("\nSuccess, tag contains an empty record.");        
+        } else {
+            Serial.println("\nUnable to erase tag.");
+        }
+
+    }
+    delay(5000);
+}
