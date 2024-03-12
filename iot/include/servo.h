9c193f757738b4a0ea8aac7183a7a98faf89cf75
Alexandre Bedel <alexandre.bedel@epitech.eu>
2024-03-12 19:01:49 +0100

    #14 Improved the esp32 code & trying to manage both the camera and the m5 buttons

--- iot/include/servo.h
+++ iot/include/servo.h
@@ -1,15 +1,18 @@
 #if !defined(SERVO_H)
 #define SERVO_H
 
+#include <stdint.h>
+#include <map>
+#include <GoPLUS2.h>
+
 #define OPEN_ANGLE 90
 #define CLOSE_ANGLE 5
 
-enum BOXES
-{
-
-};
-
-#include <stdint.h>
+/**
+ * Maps the Servo numbers to the given
+ * trash types
+ */
+extern std::map<String, int> SERVO_BOXES;
 
 class ServoMotor
 {
@@ -17,9 +20,13 @@ private:
     static void run(uint8_t servo_num, uint8_t angle);
 
 public:
+    /**
+     * Initializes the Servo module
+     */
     static void init();
     static void open(uint8_t servoNum);
     static void close(uint8_t servoNum);
+    static void buttonsTask(void *pvParameters);
 };
 
 #endif // SERVO_H
