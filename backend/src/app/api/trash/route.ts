19b190f3b0c1c9fd6807ee65bfa4eea5ce8b78ff
JeremyElbaze <jeremy-elbaze@outlook.fr>
2024-03-14 22:35:24 +0100

    add login logic, add data keys for api/trash, add middleware for stat, start front (init shadcn) and setup login form with zod

--- backend/src/app/api/trash/route.ts
+++ backend/src/app/api/trash/route.ts
@@ -1,10 +1,48 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
+export type TrashType = "recyclable" | "trash" | "glass";
+export interface Trash {
+  id: number;
+  binId: string;
+  trashType: TrashType;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export interface Stats {
+  totalTrashByBin: { [binId: string]: number };
+  totalTrashByType: { [trashType: string]: number };
+}
+
 export async function GET() {
   try {
-    const trashData = await prisma.trash.findMany();
-    return NextResponse.json(trashData);
+    const trashData: Trash[] = await prisma.trash.findMany();
+    const totalTrash: number = trashData.length;
+
+    const responseData = {
+      trashData: trashData,
+      totalTrash: totalTrash,
+      stats: {
+        totalTrashByBin: {},
+        totalTrashByType: {
+          recyclable: 0,
+          trash: 0,
+          glass: 0
+        }
+      } as Stats
+    };
+
+    trashData.forEach(trash => {
+      const { binId, trashType } = trash;
+
+      if (responseData.stats.totalTrashByBin[binId]) responseData.stats.totalTrashByBin[binId]++
+      else responseData.stats.totalTrashByBin[binId] = 1;
+
+      if (trashType in responseData.stats.totalTrashByType) responseData.stats.totalTrashByType[trashType]++;
+    });
+
+    return NextResponse.json(responseData);
   } catch (error) {
     console.error(error);
   }
