2ed8f21d26a9a36fe1553407d971fdc85ab1499e
Alexandre Bedel <alexandre.bedel@epitech.eu>
2024-03-14 15:59:27 +0100

    #23 Improving the servo code

--- iot/src/servo.cpp
+++ iot/src/servo.cpp
@@ -4,6 +4,10 @@
 GoPlus2 goPlus;
 std::map<String, int> SERVO_BOXES = {{"recyclable", SERVO_NUM0}, {"glass", SERVO_NUM1}, {"trash", SERVO_NUM3}};
 
+bool isRecycleOpen = false;
+bool isGlassOpen = false;
+bool isTrashOpen = false;
+
 void ServoMotor::init()
 {
     goPlus.begin();
@@ -18,33 +22,85 @@ void ServoMotor::buttonsTask(void *pvParameters)
 {
     while (true)
     {
-        // Keep this on the main loop?
         M5.update();
         if (M5.BtnA.wasPressed())
         {
-            Serial.println("Opening recycable");
-            ServoMotor::open(SERVO_BOXES["recyclable"]);
+            ServoMotor::run(SERVO_BOXES["recyclable"], isRecycleOpen ? CLOSE_ANGLE : OPEN_ANGLE);
+            isRecycleOpen = !isRecycleOpen;
         }
         if (M5.BtnB.wasPressed())
         {
-            Serial.println("Opening glass");
-            ServoMotor::open(SERVO_BOXES["glass"]);
+            ServoMotor::run(SERVO_BOXES["glass"], isGlassOpen ? CLOSE_ANGLE : OPEN_ANGLE);
+            isGlassOpen = !isGlassOpen;
         }
         if (M5.BtnC.wasPressed())
         {
-            Serial.println("Opening trash");
-            ServoMotor::open(SERVO_BOXES["trash"]);
+            ServoMotor::run(SERVO_BOXES["trash"], isTrashOpen ? CLOSE_ANGLE : OPEN_ANGLE);
+            isTrashOpen = !isTrashOpen;
         }
-        vTaskDelay(400);
+        vTaskDelay(200);
     }
 }
 
 void ServoMotor::open(uint8_t servoNum)
 {
+    // bool finalStatus = false;
+
+    switch (servoNum)
+    {
+    case 0:
+    {
+        // bool status = !ServoMotor::getIsRecycleOpen();
+
+        isRecycleOpen = true;
+        // finalStatus = status;
+        // ServoMotor::setIsRecycleOpen(true);
+        break;
+    }
+    case 1:
+    {
+        // bool status = !ServoMotor::getIsGlassOpen();
+
+        isGlassOpen = true;
+        // finalStatus = status;
+        // ServoMotor::setIsGlassOpen(true);
+        break;
+    }
+    case 2:
+    {
+        // bool status = !ServoMotor::getIsTrashOpen();
+
+        isTrashOpen = true;
+        // finalStatus = status;
+        // ServoMotor::setIsTrashOpen(true);
+        break;
+    }
+    }
     ServoMotor::run(servoNum, OPEN_ANGLE);
 }
 
 void ServoMotor::close(uint8_t servoNum)
 {
+    switch (servoNum)
+    {
+    case 0:
+    {
+        isRecycleOpen = false;
+        // ServoMotor::setIsRecycleOpen(false);
+        break;
+    }
+    case 1:
+    {
+        isGlassOpen = false;
+        // ServoMotor::setIsGlassOpen(false);
+        break;
+    }
+    case 2:
+    {
+        isTrashOpen = false;
+        // ServoMotor::setIsTrashOpen(false);
+        break;
+    }
+    }
     ServoMotor::run(servoNum, CLOSE_ANGLE);
 }
