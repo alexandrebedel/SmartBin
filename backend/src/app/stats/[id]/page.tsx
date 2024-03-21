195d8fb76696b8ab56bacc3cba55608422e61056
JeremyElbaze <jeremy-elbaze@outlook.fr>
2024-03-21 10:30:47 +0100

    do api trash id

--- backend/src/app/stats/[id]/page.tsx
+++ backend/src/app/stats/[id]/page.tsx
@@ -1,14 +1,3 @@
-import PieChart from "@/app/components/Piechart";
-
-
 export default function StatsDetail({ params }: { params: { id: string } }) {
-
-  return (
-    <div>
-      <div>My Post: {params.id}</div>
-      <div>
-      <PieChart/>
-    </div>
-    </div>
-  );
+  return <div>My Post: {params.id}</div>;
 }
