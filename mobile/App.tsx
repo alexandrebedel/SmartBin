ccc0f453236b3c58b325b90eb06beaeaee4d1091
Alexandre Bedel <alexandre.bedel@epitech.eu>
2024-04-04 15:22:49 +0200

    #35 Improved the whole mobile structure

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,7 +1,4 @@
 import { Text } from "react-native";
-import { LinkingOptions, NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import * as Linking from "expo-linking";
 import {
   Quicksand_300Light,
   Quicksand_400Regular,
@@ -10,15 +7,8 @@ import {
   Quicksand_700Bold,
 } from "@expo-google-fonts/quicksand";
 import { useFonts } from "expo-font";
-import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
-import { faChartLine, faGear } from "@fortawesome/free-solid-svg-icons";
-import HomeScreen from "./HomeScreen";
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { Routes } from "./Routes";
 
-const Tab = createBottomTabNavigator();
-const linking: LinkingOptions<ReactNavigation.RootParamList> = {
-  prefixes: [Linking.createURL("/")],
-};
 const fonts = {
   Quicksand_300Light,
   Quicksand_400Regular,
@@ -33,33 +23,5 @@ export default function App() {
   if (!fontsLoaded) {
     return <Text>Loading...</Text>;
   }
-  return (
-    <NavigationContainer linking={linking}>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarActiveTintColor: "lightseagreen",
-          tabBarInactiveTintColor: "gray",
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
-
-            if (route.name === "Dashboard") {
-              iconName = focused ? faChartLine : faChartLine;
-            } else if (route.name === "Settings") {
-              iconName = focused ? faGear : faGear;
-            }
-            return (
-              <FontAwesomeIcon
-                icon={iconName as IconProp}
-                size={size}
-                color={color}
-              />
-            );
-          },
-        })}
-      >
-        <Tab.Screen name="Dashboard" component={HomeScreen} />
-        {/* <Tab.Screen name="Settings" component={QRCodeScreen} /> */}
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
+  return <Routes />;
 }
