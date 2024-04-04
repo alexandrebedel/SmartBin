fe0586db8bda629dd5ccfca42cdec84c2f1b3688
Tiagouu <tiago.de-sa@epitech.eu>
2024-04-04 16:26:36 +0200

    ADD login page

--- mobile/screens/HomeScreen.tsx
+++ mobile/screens/HomeScreen.tsx
@@ -5,6 +5,8 @@ import {
   ScrollView,
   Dimensions,
   Button,
+  Image,
+  Pressable,
 } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import * as Progress from "react-native-progress";
@@ -26,12 +28,59 @@ export default function HomeScreen() {
 
   if (!id) {
     return (
-      <View>
-        <Text>No such bin id found</Text>
-        <Button
-          title="Scan your bin QR code"
+      <View style={{ flex: 1, padding: 15, backgroundColor: "white" }}>
+        <StatusBar style="auto" />
+        <Text
+          style={{
+            fontFamily: "Quicksand_700Bold",
+            fontSize: 40,
+            textAlign: "center",
+            marginBottom: 20,
+          }}
+        >
+          SMART BIN
+        </Text>
+        <Image
+          source={require("../assets/logo.png")}
+          style={{
+            width: 300,
+            height: 250,
+            alignSelf: "center",
+            marginLeft: 50,
+            marginBottom: 40,
+          }}
+        ></Image>
+        <Text
+          style={{
+            fontFamily: "Quicksand_500Medium",
+            fontSize: 16,
+            textAlign: "center",
+            marginBottom: 20,
+          }}
+        >
+          Veuillez scanner le QR Code situé sur l'écran de la poubelle pour
+          vous connecter
+        </Text>
+        <Pressable
           onPress={() => navigation.navigate("Scanner")}
-        />
+          style={{
+            backgroundColor: "lightseagreen",
+            paddingVertical: 10,
+            paddingHorizontal: 15,
+            borderRadius: 10,
+            alignSelf: "center",
+          }}
+        >
+          <Text
+            style={{
+              color: "white",
+              fontFamily: "Quicksand_600SemiBold",
+              fontSize: 20,
+            }}
+          >
+            Se Connecter
+          </Text>
+        </Pressable>
       </View>
     );
   }
