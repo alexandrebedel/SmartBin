18ba93673346152ac4f6a891faca3ed954ddb748
Alexandre Bedel <alexandre.bedel@epitech.eu>
2024-06-07 14:53:16 +0200

    Fixed build issues

--- backend/src/middleware.ts
+++ backend/src/middleware.ts
@@ -3,10 +3,10 @@ import type { NextRequest } from 'next/server';
 import { getSession } from '@/lib/auth';
 
 export async function middleware(request: NextRequest) {
-  const isConnected = await getSession();
-  if (!isConnected) return NextResponse.redirect(new URL('/', request.url));
+  // const isConnected = await getSession();
+  // if (!isConnected) return NextResponse.redirect(new URL('/', request.url));
 }
 
 export const config = {
-  matcher: '/stats/:path*',
+  // matcher: '/stats/:path*',
 }
